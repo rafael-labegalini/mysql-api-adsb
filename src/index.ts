723e081da120a8e976e228e60bd2662ac257875d
Rafael <rafael@example.com>
2024-09-18 22:43:31 -0300

    listagem de categorias

--- src/index.ts
+++ src/index.ts
@@ -27,4 +27,24 @@ app.get('/categories', async function (req: Request, res: Response) {
     });
 });
 
+app.get("/categories/form", async function (req: Request, res: Response) {
+    return res.render("categories/form");
+});
+
+app.post("/categories/save", async function(req: Request, res: Response) {
+    const body = req.body;
+    const insertQuery = "INSERT INTO categories (name) VALUES (?)";
+    await connection.query(insertQuery, [body.name]);
+
+    res.redirect("/categories");
+});
+
+app.post("/categories/delete/:id", async function (req: Request, res: Response) {
+    const id = req.params.id;
+    const sqlDelete = "DELETE FROM categories WHERE id = ?";
+    await connection.query(sqlDelete, [id]);
+
+    res.redirect("/categories");
+});
+
 app.listen('3000', () => console.log("Server is listening on port 3000"));
